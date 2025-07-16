68ed7a0fcdb6a778308791c139166f5abb600ecc
YSBXS <160263476+YSBXS@users.noreply.github.com>
2025-07-16 18:55:47 +0800

    Update release.bat
    百晓生助手

--- apk/release.bat
+++ apk/release.bat
@@ -14,3 +14,11 @@ rmdir /s /q .\mobileJavaArm64_v8a
 rmdir /s /q .\mobileJavaArmeabi_v7a
 rmdir /s /q .\mobilePythonArm64_v8a
 rmdir /s /q .\mobilePythonArmeabi_v7a
+move .\leanbackArm64_v8a\release\*.apk .\release
+move .\leanbackArmeabi_v7a\release\*.apk .\release
+move .\mobileArm64_v8a\release\*.apk .\release
+move .\mobileArmeabi_v7a\release\*.apk .\release
+rmdir /s /q .\leanbackArm64_v8a
+rmdir /s /q .\leanbackArmeabi_v7a
+rmdir /s /q .\mobileArm64_v8a
+rmdir /s /q .\mobileArmeabi_v7a
